9c9f6bba19631ddf8cfa89f6cb9a5410536868ac
Raviteja02 <satyasairaviteja05@gmail>
2019-01-19 12:32:09 +0530

    save changes

--- venv/mutableimmutable.py
+++ venv/mutableimmutable.py
@@ -1,13 +1,27 @@
 x = 100;
 print (x)
-print (id (x))
+print (id (x)) #we cant modify the values of immutable objects
 
 x = 200;
 print (x)
 print (id(x))
 
-x = 200;
-print (x)
+y = 100;   #we can't create two immutable objects with same data.
+print (y)
 
-print (id(x))
+print (id(y))  #the address of the object already created will be pointed to the  new variable.
+
+
+z = [10,20,30]
+print (z)
+print (type(z))
+print (id(z))
+
+z[1] =40
+print (z)
+print (id(z))  #we can modify the values or elements of the mutable objects.
+
+a =[10,40,30]
+print (a)
+print (id(a)) #we can create two mutable objects with same data or elements.
 
